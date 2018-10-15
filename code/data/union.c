93e82a5ff792fb58f1bcbe03487fa59fcd33952c
Francisco Sant'Anna <francisco.santanna@gmail.com>
2018-10-15 13:42:23 -0300

    ...

--- code/data/union.c
+++ code/data/union.c
@@ -3,12 +3,13 @@
 typedef union flexType {
     int   i;
     float f;
-    int   v[1000];
+    //int   v[1000];
 } Flex;
 
 int main (void) {
     Flex F;
     F.i = 999999999;
-    printf(">>> %f %ld\n", F.f, sizeof(F));
+    //printf(">>> %f %ld\n", F.f, sizeof(F));
+    printf(">>> %f\n", F.f);
     return 0;
 }
